2c30f403c0cfb4035637a55bc334c45523fdf4e1
S2somang <s2_somang@naver.com>
2023-02-11 15:14:42 +0900

    컴포넌트를 여러 컴포넌트로 분할하기

--- react-udemy03/src/components/ExpenseItem.js
+++ react-udemy03/src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 // 리액트에 있는 컴포넌트는 단지 자바스크립트 함수일 뿐!
 function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>${props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <h2>${props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
