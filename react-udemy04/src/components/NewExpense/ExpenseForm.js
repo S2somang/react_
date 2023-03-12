44c458cd94d8729316eedb68e4618db46a545126
S2somang <s2_somang@naver.com>
2023-03-12 16:18:58 +0900

    react State 양방향바인딩

--- react-udemy04/src/components/NewExpense/ExpenseForm.js
+++ react-udemy04/src/components/NewExpense/ExpenseForm.js
@@ -3,64 +3,75 @@ import { useState } from 'react';
 import './ExpenseForm.css';
  
 const ExpenseForm =() => {
-    //const [enteredTitle, setEnteredTitle] = useState('');
-    //const [enteredAmount, setEnteredAmount] = useState('');
-    //const [enteredDate , setEnteredDate] = useState('');
-    const [userInput, setUserInput] = useState({
-        enteredTitle :'',
-        enteredAmount:'',
-        enteredDate:'',
-    });
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate , setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle :'',
+    //     enteredAmount:'',
+    //     enteredDate:'',
+    // });
 
     // title이 변경될 때마다 실행될 놈
     const titleChangeHandler = (event) => {
-        //setEnteredTitle(event.target.value);
+        setEnteredTitle(event.target.value);
         // setUserInput({
         //     ...userInput,
         //     enteredTitle: event.target.value,
         // })
-        setUserInput((prevState)=> {
-            return {...prevState, enteredTitle: event.target.value};
-        })
+        // setUserInput((prevState)=> {
+        //     return {...prevState, enteredTitle: event.target.value};
+        // })
     };
 
     const amountCahngeHandler = (event) => {
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-        })
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // })
     }
 
     const dateChangerHandlrer =(event) => {
-        // setEnteredDate(evnet.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-        })
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // })
     }
     
-    const submitHandler = () => {
+    const submitHandler = (event) => {
+        event.preventDefault();
 
+        const expendsDate = {
+            title: enteredTitle,
+            amount : enteredAmount,
+            date: new Date(enteredDate),
+        };
+
+        console.log(expendsDate);
+        setEnteredTitle(''); // 입력값 초기화
+        setEnteredAmount('');
+        setEnteredDate('');
     }
 
     return <form onSubmit={submitHandler}> 
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type ='text' onChange={titleChangeHandler}/>
+                <input type ='text' value ={enteredTitle} onChange={titleChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type ='number' min='0.01' step='0.01' onChange={amountCahngeHandler}/ >
+                <input type ='number' value= {enteredAmount} min='0.01' step='0.01' onChange={amountCahngeHandler}/ >
             </div>
         </div>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type ='date' min='2022-01-01' max='2025-12-31'onChange={dateChangerHandlrer}/>
+                <input type ='date' value ={enteredDate} min='2022-01-01' max='2025-12-31'onChange={dateChangerHandlrer}/>
             </div>
         </div>
         <div className='new-expense__actions'>
